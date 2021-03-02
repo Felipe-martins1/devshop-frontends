73f495bbc845ace1b41e26ea2c640b62c12bd533
Felipe <Felipe.dom.martins@gmail.com>
2021-03-02 17:35:40 -0300

    Create Category

--- panel/pages/categories/create.js
+++ panel/pages/categories/create.js
@@ -1,6 +1,10 @@
 import React from 'react'
+
 import Layout from '../../components/Layout'
 import Title from '../../components/Title'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
 import { useMutation } from '../../lib/graphql'
 import { useFormik } from 'formik'
 import { useRouter } from 'next/router'
@@ -18,46 +22,56 @@ const CREATE_CATEGORY = `
   }
   `
 const Index = () => {
+  //Inicia o Router(Usado para redirecionar para a página /categories)
   const router = useRouter()
+
+  //UseMutation(Usando a função criada na lib para puxar a função que executa a mutation)
   const [data, createCategory] = useMutation(CREATE_CATEGORY)
+
+  //Criando form usando Formik
   const form = useFormik({
     initialValues: {
       name: '',
       slug: ''
     },
     onSubmit: async values => {
+      //Usando mutation
       await createCategory(values)
+
+      //Redirecionando
       router.push('/categories')
     }
   })
   return (
     <Layout>
       <Title>Criar nova categoria</Title>
+      <Button.Link href='/categories'>Voltar</Button.Link>
       <div className='flex flex-col mt-8'>
         <div className='-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8'>
-          <div className='align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200'></div>
-          <form onSubmit={form.handleSubmit}>
-            <input
-              className='p-2 font-semibold rounded shadow-md outline-none focus:ring-2 focus:ring-blue-600 '
-              type='text'
-              name='name'
-              onChange={form.handleChange}
-              values={form.values.name}
-            />
-            <input
-              className='p-2 ml-2 font-semibold rounded shadow-md outline-none focus:ring-2 focus:ring-blue-600'
-              type='text'
-              name='slug'
-              onChange={form.handleChange}
-              values={form.values.slug}
-            />
-            <button
-              className='ml-2 bg-indigo-600 rounded p-2 text-white font-semibold '
-              type='submit'
-            >
-              Criar categoria
-            </button>
-          </form>
+          <div className='align-middle inline-block min-w-full bg-white shadow overflow-hidden sm:rounded-lg border-b border-gray-200 p-12'>
+            <form onSubmit={form.handleSubmit}>
+              <div class='flex flex-wrap -mx-3 mb-6'>
+                <Input
+                  label='Nome da categoria'
+                  placeholder='Preencha com o nome da categoria'
+                  type='text'
+                  name='name'
+                  onChange={form.handleChange}
+                  value={form.values.name}
+                />
+                <Input
+                  label='Slug da categoria'
+                  placeholder='Preencha com o nome da categoria'
+                  type='text'
+                  name='slug'
+                  onChange={form.handleChange}
+                  value={form.values.slug}
+                  helpText='Slug é utilizado para URLs amigáveis'
+                />
+              </div>
+              <Button type='submit'>Criar categoria</Button>
+            </form>
+          </div>
         </div>
       </div>
     </Layout>
